74937cbf13d834174d35319b0841256ce99d4355
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-07-05 11:44:08 +0800

    custom: button from shadcn

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,15 +5,16 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'border-2 border-black gap-2 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+        default:
+          'bg-[#6149D4] text-primary-foreground hover:bg-[#46297B] hover:scale-[105%] transition ease-in-out delay-150',
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
-          'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
+          'bg-white text-black font-bold hover:border-[#6149D4] hover:text-[#6149D4] hover:scale-[105%] duration-300 transition ease-in-out delay-150',
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
