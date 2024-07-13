8f927600652bae5dbb084650f9fd7c5ab029c754
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-07-13 11:24:04 +0800

    add: get token function

--- src/lib/custom-fetch.ts
+++ src/lib/custom-fetch.ts
@@ -1,3 +1,5 @@
+import { jwtDecode } from 'jwt-decode'
+
 export const customFetch = async (route: string, options: RequestInit) => {
   const response = await fetch(`${process.env.API_URL}${route}`, options)
 
@@ -5,4 +7,25 @@ export const customFetch = async (route: string, options: RequestInit) => {
     throw new Error('Network response was not ok')
   }
   return response.json()
+}
+
+export function getToken():
+  | {
+      token: string
+      username: string
+      exp: string
+    }
+  | undefined {
+  const token = localStorage.getItem('token')
+  if (token) {
+    const decoded: {
+      username: string
+      exp: string
+    } = jwtDecode(token)
+    return {
+      token: token,
+      username: decoded.username,
+      exp: decoded.exp,
+    }
+  }
 }
