393fe8de3afe083e4cef77da32312cc3ffccaa2e
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-07-15 21:58:28 +0800

    feat: vote calon logic implementation

--- src/modules/VotePageModule/index.tsx
+++ src/modules/VotePageModule/index.tsx
@@ -1,20 +1,72 @@
 /* eslint-disable react/react-in-jsx-scope */
 'use client'
 import { Button } from '@/components/ui/button'
-import { fetchWithToken } from '@/custom-hook/customFetch'
+import { toast } from '@/components/ui/use-toast'
+import { fetchWithoutToken, fetchWithToken } from '@/custom-hook/customFetch'
 import useToken from '@/custom-hook/useToken'
 import { DoorClosed, Vote } from 'lucide-react'
 import Image from 'next/image'
-import { Background } from '../LandingPageModule/components/background'
-import { toast } from '@/components/ui/use-toast'
 import { useRouter } from 'next/navigation'
+import { Background } from '../LandingPageModule/components/background'
+import { VoteConfirmationDialog } from './components/vote-confirmation'
 
 export default function VotePageModule() {
-  const { token } = useToken()
+  const { token, decoded } = useToken()
   const { push } = useRouter()
 
-  async function logOut() {
-    const response = await fetchWithToken('/auth/logout', token, {
+  async function voteCalon(calonId: number) {
+    const response = await fetchWithToken(
+      `/vote/${calonId}/${decoded.id}`,
+      token,
+      {
+        method: 'POST',
+      }
+    )
+
+    if (response.message === 'User already voted') {
+      toast({
+        title: 'Vote',
+        description: 'Eits, vote sekali doang yak',
+        variant: 'destructive',
+      })
+      return
+    }
+
+    if (response.message === 'Calon not found') {
+      toast({
+        title: 'Vote',
+        description: 'Calon tidak ditemukan!',
+        variant: 'destructive',
+      })
+      return
+    }
+
+    if (response.message === 'User not found') {
+      toast({
+        title: 'Vote',
+        description: 'User tidak ditemukan!',
+        variant: 'destructive',
+      })
+      return
+    }
+
+    if (response.result) {
+      toast({
+        title: 'Vote',
+        description: 'Berhasil vote!',
+        variant: 'default',
+      })
+    } else {
+      toast({
+        title: 'Vote',
+        description: 'Gagal vote!',
+        variant: 'destructive',
+      })
+    }
+  }
+
+  async function logout() {
+    const response = await fetchWithoutToken(`/auth/logout/${decoded.id}`, {
       method: 'POST',
     })
 
@@ -33,22 +85,22 @@ export default function VotePageModule() {
   }
 
   return (
-    <section className="w-full h-screen overflow-hidden flex flex-col relative font-manrope">
+    <section className="w-full h-full overflow-hidden flex flex-col relative font-manrope">
       <Background />
-      <div className="w-full p-20 z-20 flex flex-col gap-2">
-        <Button variant={'outline'} className="w-[180px]" onClick={logOut}>
+      <div className="w-full p-10 md:p-20 z-20 flex flex-col gap-2">
+        <Button variant={'outline'} className="w-[180px]" onClick={logout}>
           <DoorClosed />
           <span>Logout</span>
         </Button>
         <h1 className="font-bold font-manrope text-3xl">
           Pilih Masa Depan Smanika
         </h1>
-        <div className="flex justify-between w-full h-full font-manrope">
+        <div className="flex flex-col lg:flex-row  gap-10 justify-between w-full font-manrope">
           {[1, 2, 3].map((item) => {
             return (
               <div
                 key={item}
-                className="bg-white w-96 rounded-lg shadow-lg p-10 flex flex-col gap-4 border-2 border-black"
+                className="bg-white w-full lg:w-96 rounded-lg shadow-lg p-10 flex flex-col gap-4 border-2 border-black"
               >
                 <div className="w-full flex justify-center">
                   <Image
@@ -64,10 +116,12 @@ export default function VotePageModule() {
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Doloremque, quos.
                 </p>
-                <Button className="w-full">
-                  <Vote />
-                  <span>Vote</span>
-                </Button>
+                <VoteConfirmationDialog onSubmit={() => voteCalon(item)}>
+                  <Button className="w-full">
+                    <Vote />
+                    <span>Vote</span>
+                  </Button>
+                </VoteConfirmationDialog>
               </div>
             )
           })}
