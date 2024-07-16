f2eef90047ba1263fe81d8019e48c337a90f92ff
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-07-16 21:46:41 +0800

    feat: 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/react-in-jsx-scope */
+export default function NotFound() {
+  return (
+    <section className="h-screen relative flex items-center justify-center font-manrope">
+      <div className="flex flex-col items-center">
+        <h1 className="text-5xl font-extrabold">404</h1>
+        <h2 className="text-2xl font-semibold">Page Not Found</h2>
+        <a
+          href="/"
+          className="hover:scale-105 duration-150 hover:font-bold hover:text-[#6149D4]"
+        >
+          Redirect to Home?
+        </a>
+      </div>
+    </section>
+  )
+}
