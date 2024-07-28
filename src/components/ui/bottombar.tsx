4d69264d38af7d92c280aa2516e28631e9708d48
Rexadell <reksaparamaaditya@gmail.com>
2024-07-28 12:06:36 +0800

    footer v2

--- src/components/ui/bottombar.tsx
+++ src/components/ui/bottombar.tsx
@@ -1,10 +1,12 @@
 'use client'
-import { Facebook, Instagram, Twitter, Youtube } from 'lucide-react'
+import { Facebook, Instagram, Section, Twitter, Youtube } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
 
 export const BottomBar = () => {
   return (
+    <section>
+      <img src="blob-bottom.png" alt="blob" className='w-full h-[67px] lg:h-auto' />
     <main className="w-full bg-[#6149D4] flex flex-col gap-3 py-8 px-16 font-manrope text-white font-semibold">
       <div className="w-full flex justify-between ">
         <Image
@@ -38,6 +40,10 @@ export const BottomBar = () => {
           </a>
         ))}
       </div>
+      <div className='text-center'>
+        <h4 className='text-sm lg:text-base'>&copy; 2024 OSIS REVION, All right reserved.</h4>
+      </div>
     </main>
+    </section>
   )
 }
