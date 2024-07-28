b2c633765d074750792f5399e7d7dce88ecaa95b
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-07-28 11:05:08 +0800

    fix: push error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,21 @@ import LandingPageModule from '@/modules/LandingPageModule'
 import { FAQSection } from '@/modules/LandingPageModule/faq'
 import { TataCaraModule } from '@/modules/LandingPageModule/tata-cara'
 import { VisiMisiModule } from '@/modules/LandingPageModule/visi-misi'
+import Image from 'next/image'
 
 export default function Home() {
   ExpirationLogout()
 
   return (
     <>
+      <Image
+        src={'/pattern.png'}
+        layout="fill"
+        objectFit="cover"
+        objectPosition="center"
+        alt="Pattern"
+        className="fixed z-0 opacity-[3%]"
+      />
       <LandingPageModule />
       <VisiMisiModule />
       <TataCaraModule />
